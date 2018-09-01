2a00d786c99cd8bc381a4d0ed3dffb5a72261b31
George Sarantinos <georgesarantinos18@gmail.com>
2018-09-01 13:24:14 -0700

    Mountain design.

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const admin = require('firebase-admin');
+var serviceAccount = require('./assets/george-personal-website-033b5221eeac.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
 
 app.use(express.static('.'));
 
@@ -11,5 +23,18 @@ app.get('/', function(req, res) {
 app.get('/resume', function(req, res) {
   res.sendFile(path.join(__dirname+'/resume.html'));
 });
+
+var messagesRef = db.collection('messages');
+
+app.post('/message', function(req, res) {
+  var newMessanger = messagesRef.doc(new Date().toISOString());
+  var setMessage = newMessanger.set({
+    'name': req.body.name,
+    'email': req.body.email,
+    'message': req.body.message
+  });
+  res.send(req.body);
+});
+
 
 app.listen(process.env.PORT || 3000);
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,80 @@ let aboutClose = () => {
     about.style.display = "none";
   }, 800);
 }
+
+var fixed = false;
+var mountainBottom;
+var sunset = $("#sunset")
+var starWrapper = $(".starWrapper");
+var mountains = $('.mountains');
+var windowWidth = $(window).width();
+
+$(document).scroll(function() {
+  if($(window).scrollTop() > 548) {
+    sunset.css("position", "absolute");
+    sunset.css("margin-top", "548px");
+    var bottom = String(sunset.height());
+    starWrapper.css("margin-top", bottom+"px")
+  } else {
+    sunset.css("position", "fixed");
+    sunset.css("margin-top", "0px");
+  }
+
+  if($(window).width() > 1490) {
+    var screenWidth = $(window).width();
+    bottom = sunset.height() - ((screenWidth - 1489) / 6);
+    bottom = String(bottom);
+    starWrapper.css("margin-top", bottom+"px")
+  }
+
+  var scrollBottom = $(window).scrollTop() + $(window).height();
+  if(fixed === false) {
+    mountainBottom = mountains.offset().top + mountains.height();
+  }
+  if(scrollBottom > mountainBottom) {
+    mountains.css("position", "fixed");
+    mountains.css("margin-top", "20vh");
+    fixed = true;
+  }
+  if(scrollBottom < mountainBottom) {
+    mountains.css("position", "relative");
+    mountains.css("margin-top", "-900px");
+    fixed = false;
+  }
+});
+
+$(window).resize(function() {
+  var bottom = String(sunset.height());
+  starWrapper.css("margin-top", bottom+"px")
+  if($(window).width() > 1490) {
+    var screenWidth = $(window).width();
+    bottom = sunset.height() - ((screenWidth - 1489) / 6);
+    bottom = String(bottom);
+    starWrapper.css("margin-top", bottom+"px")
+  }
+  windowWidth = $(window).width() - windowWidth;
+  if(fixed === true) {
+    mountainBottom += windowWidth;
+  }
+});
+
+var messageVue = new Vue({
+  el: '#message',
+  data: {
+    user: {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    submitMessage: function() {
+      var messageData = {
+        name: this.user.name,
+        email: this.user.email,
+        message: this.user.message
+      }
+      // $.post("/message", messageData);
+    }
+  }
+});
